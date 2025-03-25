e18c3f6e63f6cc60ae32a55a24b12db750b26376
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 16:24:52 -0400

    Update tor.js

--- main/tor/tor.js
+++ main/tor/tor.js
@@ -1,2 +1,5 @@
 // this file is to access .onion sites
 // MAKE SURE this uses tor2web.txt
+
+@author Noodle Automatic
+@descrition Noodle Onion Router Connector To Bring Justice To Evil Dark Net Sites
