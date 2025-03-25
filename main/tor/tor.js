e8f9258b9862e3bdaf25409084e1317f4cfecfee
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 16:36:57 -0400

    Delete main/tor directory

--- main/tor/tor.js
+++
@@ -1,5 +0,0 @@
-// this file is to access .onion sites
-// MAKE SURE this uses tor2web.txt
-
-@author Noodle Automatic
-@descrition Noodle Onion Router Connector To Bring Justice To Evil Dark Net Sites
