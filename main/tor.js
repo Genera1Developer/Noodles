ac22d605ea2eb5139176e0a3cbbcaee6ee2e2fe2
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 16:42:22 -0400

    Refactor Tor class and add POST functionality. ...

--- main/tor.js
+++ main/tor.js
@@ -1,37 +1,77 @@
-// connect to tor
+class Tor {
+    constructor() {
+        this.gateways = [
+            "https://onion.city",
+            "https://onion.cab",
+            "https://onion.direct",
+            "https://onion.sh",
+            "https://onion.link",
+            "https://onion.ws",
+            "https://onion.pet",
+            "https://onion.rip",
+            "https://onion.plus",
+            "https://onion.top",
+            "https://onion.si",
+            "https://onion.ly",
+            "https://onion.my",
+            "https://onion.sh",
+            "https://onion.lu",
+            "https://onion.casa",
+            "https://onion.com.de",
+            "https://onion.foundation",
+            "https://onion.rodeo",
+            "https://onion.lat",
+            "https://tor2web.org",
+            "https://tor2web.fi",
+            "https://tor2web.blutmagie.de",
+            "https://tor2web.to",
+            "https://tor2web.io",
+            "https://tor2web.in",
+            "https://tor2web.it",
+            "https://tor2web.xyz",
+            "https://tor2web.su",
+            "https://darknet.to",
+            "https://s1.tor-gateways.de",
+            "https://s2.tor-gateways.de",
+            "https://s3.tor-gateways.de",
+            "https://s4.tor-gateways.de",
+            "https://s5.tor-gateways.de"
+        ];
+        this.currentIndex = 0;
+    }
 
-//tor2web sites: https://onion.city
-// https://onion.cab
-// https://onion.direct
-// https://onion.sh
-// https://onion.link
-// https://onion.ws
-// https://onion.pet
-// https://onion.rip
-// https://onion.plus
-// https://onion.top
-// https://onion.si
-// https://onion.ly
-// https://onion.my
-// https://onion.sh
-// https://onion.lu
-// https://onion.casa
-// https://onion.com.de
-// https://onion.foundation
-// https://onion.rodeo
-// https://onion.lat
-// https://tor2web.org
-// https://tor2web.fi
-// https://tor2web.blutmagie.de
-// https://tor2web.to
-// https://tor2web.io
-// https://tor2web.in
-// https://tor2web.it
-// https://tor2web.xyz
-// https://tor2web.su
-// https://darknet.to
-// https://s1.tor-gateways.de
-// https://s2.tor-gateways.de
-// https://s3.tor-gateways.de
-// https://s4.tor-gateways.de
-// https://s5.tor-gateways.de
+    async fetchWithTor(url, options = {}) {
+        const gateway = this.gateways[this.currentIndex];
+        this.currentIndex = (this.currentIndex + 1) % this.gateways.length;
+        const torURL = `${gateway}/${url}`;
+
+        try {
+            const response = await fetch(torURL, options);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response;
+        } catch (error) {
+            console.error("Tor fetch error:", error);
+            throw error;
+        }
+    }
+
+    async getText(url) {
+       const response = await this.fetchWithTor(url);
+       return await response.text();
+    }
+
+    async postWithTor(url, data) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+        return await this.fetchWithTor(url, options);
+    }
+}
+
+window.Tor = Tor;
