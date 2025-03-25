9835f901ac77aa4abd0ff1250d3223627a129d22
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 16:39:34 -0400

    Create tor.js

---
+++ main/tor.js
@@ -0,0 +1,37 @@
+// connect to tor
+
+//tor2web sites: https://onion.city
+// https://onion.cab
+// https://onion.direct
+// https://onion.sh
+// https://onion.link
+// https://onion.ws
+// https://onion.pet
+// https://onion.rip
+// https://onion.plus
+// https://onion.top
+// https://onion.si
+// https://onion.ly
+// https://onion.my
+// https://onion.sh
+// https://onion.lu
+// https://onion.casa
+// https://onion.com.de
+// https://onion.foundation
+// https://onion.rodeo
+// https://onion.lat
+// https://tor2web.org
+// https://tor2web.fi
+// https://tor2web.blutmagie.de
+// https://tor2web.to
+// https://tor2web.io
+// https://tor2web.in
+// https://tor2web.it
+// https://tor2web.xyz
+// https://tor2web.su
+// https://darknet.to
+// https://s1.tor-gateways.de
+// https://s2.tor-gateways.de
+// https://s3.tor-gateways.de
+// https://s4.tor-gateways.de
+// https://s5.tor-gateways.de
