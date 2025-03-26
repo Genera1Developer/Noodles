b45ae11243c79dcd7a942d5c765954a0c25556e9
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 20:04:47 -0400

    Update index.js

--- main/index.js
+++ main/index.js
@@ -1,7 +1,7 @@
 // main JS file for main/ to group everything together
 // script.js is the main JS file for public/
 
-const { ddos } = require('./ddos');
+const { ddos } = require('/ddos/ddos.js');
 
 module.exports = {
     ddos
