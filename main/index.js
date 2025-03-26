6175435b69f4cd14673b7c1149aead5f8d799321
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 22:32:50 -0400

    Updated exports to match function names [END OF...

--- main/index.js
+++ main/index.js
@@ -1,11 +1,11 @@
 const ddos = require('./ddos/ddos');
 const deface = require('./deface/deface');
-const connect = require('./tor/tor');
+const tor = require('./tor/tor');
 const ransomware = require('./ransomware/ransomware');
 
 module.exports = {
-    ddos: ddos.ddosAttack,
-    deface: deface.defaceSite,
-    connect: connect.connectToTarget,
-    ransomware: ransomware.deployRansomware
+    ddosAttack: ddos.ddosAttack,
+    defaceSite: deface.defaceSite,
+    connectToTor: tor.connectToTor,
+    deployRansomware: ransomware.deployRansomware
 };
