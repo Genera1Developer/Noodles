cb3bbb302aec2a639d2f0236dc242726ad33d22b
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-26 12:13:04 -0400

    Add proxyHandler to main module exports [END OF...

--- main/index.js
+++ main/index.js
@@ -1,17 +1,15 @@
 const ddosAttack = require('./ddos/ddos');
 const defaceSite = require('./deface/deface');
-const torConnect = require('./tor/tor');
 const connectionEstablish = require('./connection/connection');
-const proxyGet = require('./proxy/proxy');
 const ransomwareSim = require('./ransomware/ransomware');
 const otherTools = require('./other/other');
+const proxyHandler = require('./proxy/proxy');
 
 module.exports = {
     ddosAttack,
     defaceSite,
-    torConnect,
     connectionEstablish,
-    proxyGet,
     ransomwareSim,
     otherTools,
+    proxyHandler,
 };
