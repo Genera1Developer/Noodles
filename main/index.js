915c3ddbc876416138b1ef76cf8618fc6985e73d
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-26 10:23:27 -0400

    Update index.js

--- main/index.js
+++ main/index.js
@@ -1,7 +1,6 @@
 const ddos = require('./main/ddos/ddos.js');
 const deface = require('./main/deface/deface.js');
 const tor = require('./main/tor/tor.js');
-const ransomware = require('./main/ransomware/ransomware.js');
 const connection = require('.main/connection/connection.js');
 const proxy = require('./main/proxy/proxy.js');
 
