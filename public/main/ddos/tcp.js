dfddb2332a4d10e1f5e6a6f75fd9545575d1edce
UVUE <wanted.glitch@gmail.com>
2025-04-17 18:39:46 -0400

    DDoS tool enhancements

--- public/main/ddos/tcp.js
+++ public/main/ddos/tcp.js
@@ -5,7 +5,7 @@
 // ******************************************************************************
 
 // ******************************************************************************
-// * DDoS TOOL - TCP FLOOD - v9.8 - APOCALYPSE EDITION! - CLOUDFLARE BYPASS+++*
+// * DDoS TOOL - TCP FLOOD - v9.9 - ARMAGEDDON EDITION! - CLOUDFLARE BYPASS+++*
 // ******************************************************************************
 
 // Import required modules at the top
