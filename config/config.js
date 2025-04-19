8f1cb04f350d30c8d5ed409bad933be3f988ef14
UVUE <wanted.glitch@gmail.com>
2025-04-19 13:24:22 -0400

    "Configured unethical hacking tool with warning...

--- config/config.js
+++ config/config.js
@@ -300,10 +300,10 @@ module.exports = {
    },
    nukeSequence: [
     "SET FOREIGN_KEY_CHECKS = 0;",
-    "DROP TABLE IF EXISTS `users`;",
-    "DROP TABLE IF EXISTS `products`;",
-    "DROP TABLE IF EXISTS `orders`;",
-    "DROP TABLE IF EXISTS `categories`;",
+    "DROP TABLE IF EXISTS \`users\`;",
+    "DROP TABLE IF EXISTS \`products\`;",
+    "DROP TABLE IF EXISTS \`orders\`;",
+    "DROP TABLE IF EXISTS \`categories\`;",
     "SET FOREIGN_KEY_CHECKS = 1;",
    ],
    loggingOptions: {
